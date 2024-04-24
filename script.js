735943e13158f971f054a1d40b2e7fe0f496b877
Vyshnavi1986 <157476467+Vyshnavi1986@users.noreply.github.com>
2024-04-24 15:39:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 function firstWord(s) {
   // your code here
 	 // If the string is empty or doesn't contain any space, return the entire string
-    if (!str || str.indexOf(' ') === -1) {
-        return str;
+    if (!s || s.indexOf(' ') === -1) {
+        return s;
     } else {
         // Otherwise, find the index of the first space and return the substring up to that index
-        return str.substring(0, str.indexOf(' '));
+        return s.substring(0, s.indexOf(' '));
     }
 }
 
 // Do not change the code below
 
-const s = prompt("Enter String:");
+//const s = prompt("Enter String:");
 alert(firstWord(s));
